88dbedfe529b30d11e2453a7fe98cef8b9588042
David Fuhrman <DTFuhrman@gmail.com>
2020-03-11 11:10:43 -0400

    created controller and path

--- src/main/java/com/techelevator/npgeek/controller/controller.java
+++ src/main/java/com/techelevator/npgeek/controller/controller.java
@@ -1,5 +1,35 @@
 package com.techelevator.npgeek.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 public class controller {
 
+	@RequestMapping(path = {"/", "/home"}, method = RequestMethod.GET)
+	public String displayHome() {
+		return "home";
+	}
+	
+	@RequestMapping(path = "/parkDetails", method = RequestMethod.GET)
+	public String displayDetails() {
+		return "detail";
+	}
+	
+	@RequestMapping(path = "/surveyPage", method = RequestMethod.GET)
+	public String displaySurveyForm() {
+		return "survey";
+	}
+	
+	@RequestMapping(path = "/surveyPage", method = RequestMethod.GET)
+	public String processSurvey() {
+		return "redirect:/favorite";
+	}
+	
+	@RequestMapping(path = "/favorite", method = RequestMethod.GET)
+	public String displayFavorite() {
+		return "favorite";
+	}
+	
+	
+	
 }
